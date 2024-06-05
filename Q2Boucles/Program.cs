0067c1059abb752620b483051f3273aece1353e8
ABT Aurelien <aurelien@abrassart.net>
2024-06-05 12:19:46 +0200

    Ajoutez des fichiers projet.

---
+++ Q2Boucles/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        string[] moves = new string[32];
+
+        for (int i = 0; i < moves.Length; i++)
+        {
+            Console.WriteLine("Entre un pas de danse (mettre done pour arrêter la saisie) : ");
+            string pas = Console.ReadLine();
+
+            if (pas == "done")
+            {
+                break;
+            } else
+            {
+                moves[i] = pas;
+            }
+        }
+
+        Console.WriteLine("Nombre d'itération ?");
+        int interation = Convert.ToInt32(Console.ReadLine());
+        for (int i = 0;i < interation; i++)
+        {
+            foreach (string move in moves) {
+                Console.Write($"{move} ");
+                //Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
